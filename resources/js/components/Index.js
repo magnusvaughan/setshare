3ca6419f57b541ccd363e798077ee4e04626d9b5
mvaughan <magnus.vaughan@gmail.com>
2020-08-29 13:54:13 +0100

    Added React Router

--- resources/js/components/Index.js
+++ resources/js/components/Index.js
@@ -1,98 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './Home'
+import Sets from './Sets'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 function Index() {
     return (
-        <div class="flex items-center">
-            <div class="md:w-1/2 md:mx-auto">
-                <div class="py-12 bg-white">
-                    <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-                        <div class="lg:text-center">
-                            <p class="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Share your sets</p>
-                            <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
-                                Upload and download great Ableton live sets
-                    </h3>
-                            <p class="mt-4 max-w-2xl text-xl leading-7 text-gray-500 lg:mx-auto">
-                                Techno, techno techno, techno.
-                    </p>
-                        </div>
 
-                        <div class="mt-10">
-                            <ul class="md:grid md:grid-cols-2 md:col-gap-8 md:row-gap-10">
-                                <li>
+        <div>
+
+            <Router>
+                <nav class="bg-indigo-500">
+                    <div class="md:w-1/2 md:mx-auto">
+                        <div class="relative flex items-center justify-between h-16">
+                            <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                                <button class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white transition duration-150 ease-in-out" aria-label="Main menu" aria-expanded="false">
+                                    <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                                    </svg>
+                                    <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
+                            </div>
+                            <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                                <div class="hidden sm:block sm:ml-6">
                                     <div class="flex">
-                                        <div class="flex-shrink-0">
-                                            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                                                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
-                                                </svg>
-                                            </div>
-                                        </div>
-                                        <div class="ml-4">
-                                            <h4 class="text-lg leading-6 font-medium text-gray-900">Ableton-only projects keep file size small</h4>
-                                            <p class="mt-2 text-base leading-6 text-gray-500">
-                                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
-                                    </p>
-                                        </div>
+                                        <Link to="/" className="px-3 py-2 rounded-md text-sm font-medium leading-5 text-white bg-indigo-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Home</Link>
+                                        <Link to="/sets" class="ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">All sets</Link>
                                     </div>
-                                </li>
-                                <li class="mt-10 md:mt-0">
-                                    <div class="flex">
-                                        <div class="flex-shrink-0">
-                                            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                                                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
-                                                </svg>
-                                            </div>
-                                        </div>
-                                        <div class="ml-4">
-                                            <h4 class="text-lg leading-6 font-medium text-gray-900">Filter by genre, bpm, and more</h4>
-                                            <p class="mt-2 text-base leading-6 text-gray-500">
-                                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
-                                    </p>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="mt-10 md:mt-0">
-                                    <div class="flex">
-                                        <div class="flex-shrink-0">
-                                            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                                                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
-                                                </svg>
-                                            </div>
-                                        </div>
-                                        <div class="ml-4">
-                                            <h4 class="text-lg leading-6 font-medium text-gray-900">Find like-minded people to make music with</h4>
-                                            <p class="mt-2 text-base leading-6 text-gray-500">
-                                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
-                                    </p>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="mt-10 md:mt-0">
-                                    <div class="flex">
-                                        <div class="flex-shrink-0">
-                                            <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                                                <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-                                                </svg>
-                                            </div>
-                                        </div>
-                                        <div class="ml-4">
-                                            <h4 class="text-lg leading-6 font-medium text-gray-900">Learn from the best.</h4>
-                                            <p class="mt-2 text-base leading-6 text-gray-500">
-                                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
-                                    </p>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+
+                    <div class="hidden sm:hidden">
+                        <div class="px-2 pt-2 pb-3">
+                            <a href="#" class="block px-3 py-2 rounded-md text-base font-medium text-white bg-gray-900 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">Home</a>
+                            <a href="#" class="mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out">All sets</a>
+                        </div>
+                    </div>
+                </nav>
+
+                <Switch>
+                    <Route path="/sets">
+                        <Sets />
+                    </Route>
+                    <Route path="/">
+                        <Home />
+                    </Route>
+                </Switch>
+            </Router>
         </div>
+
     );
 }
 
---
+++ resources/js/components/Sets.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Sets() {
+    return(
+        <h1>Set list</h1>
+    )
+}
+
+export default Sets;
