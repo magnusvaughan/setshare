3dd08ddf823aacf7b2460dff620f442d53c6416b
mvaughan <magnus.vaughan@gmail.com>
2020-08-29 13:19:26 +0100

    Created Set model, migration, controller and resource

---
+++ database/migrations/2020_08_29_115430_create_sets_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sets', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name', 300);
+            $table->integer('user_id')->unsigned();
+            $table->integer('bpm');
+            $table->string('download_url', 300);
+        });
+
+        Schema::table('sets', function($table) {
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sets');
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\Set as SetResource;
+use App\Set;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/set', function () {
+    return SetResource::collection(Set::all());
 });
